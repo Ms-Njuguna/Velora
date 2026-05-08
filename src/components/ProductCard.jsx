56e6f24449ac64cf926c5f0792ec91d08f86249d
Patricia <patricianjuguna084@gmail.com>
2026-05-08 15:22:04 +0300

    chore(productCard): add prop validation to price prop

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,10 @@
 export default function ProductCard({title, price, imageURL}) {
+    const validatePrice = typeof price === "number" && !isNaN(price) ? price : 0;
+
     return (
         <div>
             <h1>{title}</h1>
-            <p>{price}</p>
+            <p>kes: {validatePrice.toFixed(2)}</p>
             <img alt="product image" src={imageURL}/>
         </div>
     )
